7a2633737d6a141c068c033b78df948504e3a0e9
karysdove2022 <dove.karys@icloud.com>
2026-01-13 12:52:30 -0600

    server updated to handle new API endpoints

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('.'));
 
 function readData(){
   try{ return JSON.parse(fs.readFileSync(DATA_FILE,'utf8') || '[]'); }
